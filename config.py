013b5e9076fa2188c6bd1187b43928f5f26a75b7
Anthony <gamerseth@protonmail.com>
2020-05-27 17:38:37 -0400

    oops

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import psycopg2
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -14,4 +13,6 @@ class Config(object):
     MAIL_USERNAME = "apikey"
     MAIL_PASSWORD = os.environ.get('SENDGRID_API_KEY')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
-
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
